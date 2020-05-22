56676935d6b0370a3d7bcc20ab2e8d5a8492d7bc
jayant45 <jayantdeidara19@gmail.com>
2020-05-22 21:35:56 +0530

    Corrected Tests

--- contest/urls.py
+++ contest/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, include
 from .views import contestsView,createContestView,submitContestView,allContestView,contestView,showProblemView,submitProblemView,rankListView
 
 urlpatterns = [
-    path('',contestsView),
-    path('create/',createContestView),
-    path('submit/',submitContestView),
-    path('all/',allContestView),
-    path('<int:contest_id>/',contestView),
-    path('<int:contest_id>/<int:problem_id>/',showProblemView),
-    path('<int:contest_id>/<int:problem_id>/submit/',submitProblemView),
-    path('<int:contest_id>/ranklist/',rankListView),
+    path('',contestsView,name='initialContestUrl'),
+    path('create/',createContestView,name='createContest'),
+    path('submit/',submitContestView,name='submitContest'),
+    path('all/',allContestView,name='allContest'),
+    path('<int:contest_id>/',contestView,name='contest'),
+    path('<int:contest_id>/<int:problem_id>/',showProblemView,name='showContestProblem'),
+    path('<int:contest_id>/<int:problem_id>/submit/',submitProblemView,name='submitContestProblem'),
+    path('<int:contest_id>/ranklist/',rankListView,name='rankList'),
 ]
--- problems/urls.py
+++ problems/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from .views import correctFormView,dashboardView,problemsView,showProblemView,submitProblemView,createProblemView,aceView
 
-urlpatterns = [ path('',dashboardView),
-path('thanks/',correctFormView),
+urlpatterns = [ path('',dashboardView,name='dashboard'),
+path('thanks/',correctFormView,name='correctForm'),
 path('submit/', correctFormView),
-path('problems/',problemsView),
-path('problems/<int:problem_id>/',showProblemView),
-path('submission/<int:problem_id>/',submitProblemView),
-path('createProblem/',createProblemView),
+path('problems/',problemsView,name='problems'),
+path('problems/<int:problem_id>/',showProblemView,name='showProblem'),
+path('submission/<int:problem_id>/',submitProblemView,name='submitProblem'),
+path('createProblem/',createProblemView,name='createProblem'),
 path('ace/',aceView),
 ]
