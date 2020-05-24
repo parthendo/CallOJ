8cf47dc3b9c9a3baddc5a892b04e7ba1c16949b6
jayant45 <jayantdeidara19@gmail.com>
2020-05-24 18:46:43 +0530

    User Playlist And Data Tables Added

--- problems/models.py
+++ problems/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class AllProblems(models.Model):
@@ -22,6 +23,14 @@ class Question(models.Model):
     editorialist = models.CharField(max_length=20)
     totalAttempts = models.IntegerField()
     successfulAttempts = models.IntegerField()
+
+class PlaylistProblems(models.Model):
+    problemOfUser = models.ForeignKey(User,default=None,on_delete=models.CASCADE)
+    problemLink = models.CharField(max_length=100)
+    problemName = models.CharField(max_length=100)
+    contestName = models.CharField(max_length=100)
+    difficultyLevel = models.IntegerField()
+ 
 
 
 
--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import profileView,updateProfileView,changesToProfileView
+from .views import profileView,updateProfileView,changesToProfileView,showUserProfileView,playlistCategoryProblemsView,addCategoryView,addQuestionToPlaylistView
 
-urlpatterns = [ path('',profileView),
+urlpatterns = [ path('',profileView,name='initialProfile'),
+path('addCategory/',addCategoryView),
+path('playlist/<str:playlistCategory>/questions/',playlistCategoryProblemsView,name='categoryQuestions'),
+path('<str:playlistCategory>/addQuestion/',addQuestionToPlaylistView),
 path('updateProfile/',updateProfileView),
 path('changesToProfile/',changesToProfileView),
+path('<str:searchedUser>/',showUserProfileView),
 ]
